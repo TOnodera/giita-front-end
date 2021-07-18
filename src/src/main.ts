5782ee9ef8ec2cde9d2e2028acbbd4a056b69e5f
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-18 01:44:26 +0000

    bulmaインストール

--- src/src/main.ts
+++ src/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import PirmeVue from "primevue/config";
+import "bulma/css/bulma.min.css";
 
 createApp(App).use(router).use(PirmeVue).mount("#app");
