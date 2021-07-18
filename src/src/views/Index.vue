99b6896627d00c19fd028a11de589fe0dcd06696
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-18 13:59:56 +0000

    不要なコメント削除

--- src/src/views/Index.vue
+++ src/src/views/Index.vue
@@ -78,7 +78,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Dashboard from "@/components/Dashboard.vue"; // @ is an alias to /src
+import Dashboard from "@/components/Dashboard.vue";
 import LoginGithubModalCard from "@/components/parts/ModalCard/LoginGithubModalCard.vue";
 
 export default defineComponent({
