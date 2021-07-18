88d99ffabf01da3fdbb93a4efcb2fea3069a7ed6
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-07-18 14:54:05 +0900

    Header,Footer追加

---
+++ src/src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+        <div class="notification is-primary">
+        This container is <strong>centered</strong> on desktop and larger viewports.
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: 'Header'  
+})
+</script>
--- src/src/components/layout/default.vue
+++ src/src/components/layout/default.vue
@@ -2,15 +2,12 @@
   <div>
     <div class="columns">
       <div class="column is-2">
-        <sidebar />
+        <Sidebar />
       </div>
-      <div class="column is-10">
-        <div class="container">
-          <div class="notification is-primary">
-            This container is <strong>centered</strong> on desktop and larger viewports.
-          </div>
-        </div>
+      <div class="column is-10 main-contents">
+        <Header />
         <slot></slot>
+        <Footer />
       </div>
     </div>
   </div>
@@ -18,10 +15,20 @@
 
 <script>
 import { defineComponent } from "vue";
+import Header from "../layout/Header";
 import Sidebar from "../layout/Sidebar";
+import Footer from "../layout/Footer";
 export default defineComponent({
   components: {
+    Header,
     Sidebar,
+    Footer
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.main-contents{
+  min-height: 100vh;
+}
+</style>
